62d0395da3fe68c54b4435303098a9d83d3616c5
Nick Hall <iamssb@gmail.com>
2014-07-18 16:11:01 +0900

    Trying to learn WPF

---
+++ Task Timer/Task Timer/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Task_Timer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (Process process in Process.GetProcesses())
+            {
+                ListBoxItem item = new ListBoxItem();
+                item.Content = process.ProcessName;
+                ProcessBox.Items.Add(item);
+            }
+        }
+    }
+}
